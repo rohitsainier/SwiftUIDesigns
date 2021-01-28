912e9f479b3f0f9d27d266beca632b93a70d93a0
rohitsainier <rohitsainier@gmail.com>
2021-01-28 12:36:00 +0530

    Initial Commit

---
+++ SwiftUIDesigns/SwiftUIDesignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignsApp.swift
+//  SwiftUIDesigns
+//
+//  Created by Rohit Saini on 28/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
